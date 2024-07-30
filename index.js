ec120825c9e4670fcdc2527b3fecf2fe8aa97b9b
Leandro Costa <leandro.costa@ifba.edu.br>
2024-07-30 16:37:53 -0300

    card

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 import PetCard from "./pet-card/index.js";
 
 window.customElements.define("pet-card", PetCard);
+
+
+document.querySelector("#pet1").name="FFFF"
+document.querySelector("#pet1").age = "14"
+//document.querySelector("#pet1").changedname("aaaaaaaaaaaaa")
+
+var pet = new PetCard("asdas","images/retriever.jpeg");
+//pet.name = "aaaaaa";
+pet.breed = "breed";
+pet.age = "4"
+
+
+document.querySelector(".app").appendChild(pet);
--- pet-card/index.js
+++ pet-card/index.js
@@ -20,56 +20,124 @@ template.innerHTML = `
     </div>
 `;
 class PetCard extends HTMLElement {
-  constructor() {
+
+  #name;
+  #avatar;
+  #showInfo;
+  #breed;
+  #age;
+
+  
+  constructor(name,avatar) {
     super();
-    this.showInfo = false;
+    this.#showInfo = false;
     this.attachShadow({ mode: "open" });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
+    //console.log(name)
+    if(name){
+      this.name = name;
+    }
+    if(avatar){
+      this.avatar = avatar;
+    }
   }
 
-  toggleInfo = () => {
-    this.showInfo = !this.showInfo;
-    this.shadowRoot.querySelector(".info").style.display = this.showInfo
+  get breed(){
+    return this.#breed
+  }
+
+  set breed(newValue){
+    this.#breed = newValue;
+    this.shadowRoot.querySelector('slot[name=breed]').outerHTML = this.#breed;
+  }
+
+  get age(){
+    return this.#age
+  }
+
+  set age(newValue){
+    this.#age = newValue;
+    this.shadowRoot.querySelector('slot[name=age]').outerHTML = this.#age;
+  }
+
+
+  #toggleInfo = () => {
+    this.#showInfo = !this.#showInfo;
+    this.shadowRoot.querySelector(".info").style.display = this.#showInfo
       ? "block"
       : "none";
-    this.shadowRoot.querySelector("#toggle").innerHTML = this.showInfo
+    this.shadowRoot.querySelector("#toggle").innerHTML = this.#showInfo
       ? "Hide Details"
       : "View Details";
   };
+
+  get name() {
+    return this.#name
+  }
+  
+  set name(newValue) {
+    this.setAttribute("name", newValue);
+  }
+  
+  #changedname(newValue){
+    this.#name = newValue;
+    this.shadowRoot.querySelector(".details h2").innerText = newValue;
+    this.shadowRoot.querySelector(".avatar img").alt = newValue;    
+  }
+
+  get avatar() {
+    return this.#avatar;
+  }
+
+  #changedavatar(newValue){
+    this.#avatar = newValue;
+    this.shadowRoot.querySelector(".avatar img").src = newValue;
+  }
+  
+  set avatar(newValue) {
+    this.setAttribute("avatar", newValue);
+  }
+
 
   static get observedAttributes() {
     return ["name", "avatar"];
   }
 
-  attributeChangedCallback(name, oldValue, newValue) {
-    this.shadowRoot.querySelector(".details h2").innerText =
-      this.getAttribute("name");
-    this.shadowRoot.querySelector(".avatar img").src =
-      this.getAttribute("avatar");
-    this.shadowRoot.querySelector(".avatar img").alt =
-      this.getAttribute("name");
+  attributeChangedCallback(attributeName, oldValue, newValue) {
+    if(attributeName =="name"){
+      this.#changedname(newValue);
+    }      
+    if(attributeName =="avatar"){
+      this.#changedavatar(newValue);
+    }    
+    console.log("Changed " + attributeName + " from " + oldValue + " to " + newValue);
   }
+
 
   connectedCallback() {
+    console.log("connectedCallback");
     this.shadowRoot
       .querySelector("#toggle")
-      .addEventListener("click", this.toggleInfo);
+      .addEventListener("click", this.#toggleInfo);
     this.shadowRoot
       .querySelector("#greet")
-      .addEventListener("click", () =>
-        window.alert(`Hey there! I'm ${this.getAttribute("name")}`)
-      );
+      .addEventListener("click", this.seyHi.bind(this));
   }
 
+  seyHi() {
+    console.log("seyHi");
+    window.alert(`Hey there! I'm ${this.name}`);
+  }
+
+
   disconnectedCallback() {
+    console.log("disconnectedCallback");
     this.shadowRoot
       .querySelector("#toggle")
       .removeEventListener("click", this.toggleInfo);
     this.shadowRoot
       .querySelector("#greet")
-      .removeEventListener("click", () =>
-        window.alert(`Hey there! I'm ${this.getAttribute("name")}`)
-      );
+      .removeEventListener("click", this.seyHi.bind(this));
   }
 }
 
